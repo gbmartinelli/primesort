fc7042568803e90e2c8c5af839e30b772b49c5f5
gbmartinelli <gbmartinelli@hotmail.com>
2017-11-18 16:28:30 -0200

    Programa finalizado

--- src/main.c
+++ src/main.c
@@ -1,17 +1,50 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
 #include <stdio.h>
 
 int main() {
 
-  int x, y;
+int num, flag = 0, tam_vetnum=0, j=0, k=0, tam_vetnprimos=0, div=0, aux, troca=1;
+int vet_num[1000000], vet_nprimos[1000000];
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+while (flag != 1) {
+
+  	scanf("%d", &num);
+	
+	if (num != -1) {
+		vet_num[tam_vetnum] = num;
+		tam_vetnum++;
+	}
+	else {
+		flag = 1;
+	}
+}
+
+for(j=0;j<tam_vetnum;j++){
+	for (k=1;k<=vet_num[j]/2;k++){
+		if(vet_num[j]%k == 0){
+			div++;
+		}
+	}
+	if(div != 1) {
+		vet_nprimos[tam_vetnprimos] = vet_num[j];
+		tam_vetnprimos++;
+	}
+	div = 0;
+}
+
+for(j=tam_vetnprimos-1;(j>=1)&&(troca==1);j--){
+troca=0;
+	for(k=0;k<j;k++){
+		if(vet_nprimos[k]<vet_nprimos[k+1]){
+			aux = vet_nprimos[k];
+			vet_nprimos[k] = vet_nprimos[k+1];
+			vet_nprimos[k+1] = aux;
+			troca=1;
+		}
+	}
+}
+
+for(j=0;j<tam_vetnprimos;j++){
+	printf("%d\n", vet_nprimos[j]);
+}
+return 0;
 }
